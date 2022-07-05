331da7c08c1f211b6b5bbb0d5ffa43041443ff87
“Vikroriiagit config --global user email iraruslan1035@gmail.comgit config --global core.editor code <iraruslan1035@gmail.com>
2022-07-05 12:27:38 +0300

    Movie first homework done

--- MOVIE/assets/js/dist/movie.dev.js
+++ MOVIE/assets/js/dist/movie.dev.js
@@ -1,20 +1,76 @@
 "use strict";
 
+var movieItem = {
+  props: ["movie"],
+  methods: {
+    getMovieInfo: function getMovieInfo(id) {
+      this.$emit("getMovieInfo", id);
+    },
+    addToFavorites: function addToFavorites(id) {
+      this.$emit("addToFavorites", id);
+    }
+  },
+  template: "#movieItem"
+};
 var App = {
   data: function data() {
     return {
       API_KEY: "bf99cef6",
       search: "",
+      year: "",
+      movieType: "Choose type",
       movieList: [],
       movieInfo: {},
-      showModal: false
+      showModal: false,
+      showFavorite: false,
+      favorites: [],
+      storage: {}
     };
+  },
+  created: function created() {
+    var localFavoritesList = localStorage.getItem("favorites list");
+    this.storage = JSON.parse(localFavoritesList);
+    localFavoritesList;
+
+    for (key in this.storage) {
+      this.favorites.push(this.storage[key]);
+    }
+  },
+  components: {
+    movieItem: movieItem
   },
   methods: {
     searchMovies: function searchMovies() {
       var _this = this;
 
-      if (this.search !== "") {
+      if (this.search !== "" && this.year !== "" && this.movieType !== "Choose type") {
+        // Make a request for a user with a given ID
+        axios.get("http://www.omdbapi.com/?apikey=".concat(this.API_KEY, "&s=").concat(this.search, "&y=").concat(this.year, "&type=").concat(this.movieType)).then(function (response) {
+          _this.movieList = response.data.Search;
+        })["catch"](function (error) {
+          // handle error
+          console.log(error);
+        }).then(function () {// always executed
+        });
+      } else if (this.search !== "" && this.year !== "") {
+        // Make a request for a user with a given ID
+        axios.get("http://www.omdbapi.com/?apikey=".concat(this.API_KEY, "&s=").concat(this.search, "&y=").concat(this.year)).then(function (response) {
+          _this.movieList = response.data.Search;
+        })["catch"](function (error) {
+          // handle error
+          console.log(error);
+        }).then(function () {// always executed
+        });
+      } else if (this.search !== "" && this.movieType !== "Choose type") {
+        // Make a request for a user with a given ID
+        axios.get("http://www.omdbapi.com/?apikey=".concat(this.API_KEY, "&s=").concat(this.search, "&type=").concat(this.movieType)).then(function (response) {
+          _this.movieList = response.data.Search;
+        })["catch"](function (error) {
+          // handle error
+          console.log(error);
+        }).then(function () {// always executed
+        });
+      } else if (this.search !== "") {
         // Make a request for a user with a given ID
         axios.get("http://www.omdbapi.com/?apikey=".concat(this.API_KEY, "&s=").concat(this.search)).then(function (response) {
           _this.movieList = response.data.Search;
@@ -27,6 +83,9 @@ var App = {
     },
     showMovieInfo: function showMovieInfo() {
       this.showModal = true;
+    },
+    showFavoriteList: function showFavoriteList() {
+      this.showFavorite = true;
     },
     getMovieInfo: function getMovieInfo(movieID) {
       var _this2 = this;
@@ -41,6 +100,36 @@ var App = {
         console.log(error);
       }).then(function () {// always executed
       });
+    },
+    addToFavorites: function addToFavorites(movieID) {
+      var movieIndex = this.movieList.findIndex(function (el) {
+        return el.imdbID === movieID;
+      });
+      var movieIndex2 = this.favorites.findIndex(function (el) {
+        return el.imdbID === movieID;
+      });
+
+      if (movieIndex2 === -1) {
+        this.favorites.push(this.movieList[movieIndex]);
+      } else {
+        this.favorites.splice(movieIndex2, 1);
+      }
+
+      localStorage.setItem("favorites list", JSON.stringify(this.favorites));
+    },
+    moviesAndFavorites: function moviesAndFavorites() {
+      var _this3 = this;
+
+      var movieArr = [];
+      this.movieList.forEach(function (el) {
+        var findFav = _this3.favorites.find(function (movie) {
+          return el.imdbID === movie.imdbID;
+        });
+
+        el.inFav = findFav !== undefined ? true : false;
+        movieArr.push(el);
+      });
+      return movieArr;
     }
   }
 };
--- MOVIE/assets/js/movie.js
+++ MOVIE/assets/js/movie.js
@@ -1,17 +1,98 @@
+const movieItem = {
+  props: ["movie"],
+  methods: {
+    getMovieInfo(id) {
+      this.$emit("getMovieInfo", id);
+    },
+    addToFavorites(id) {
+      this.$emit("addToFavorites", id);
+    },
+  },
+  template: "#movieItem",
+};
+
 const App = {
   data() {
     return {
       API_KEY: "bf99cef6",
       search: "",
+      year: "",
+      movieType: "Choose type",
       movieList: [],
       movieInfo: {},
-      showModal:false,
+      showModal: false,
+      showFavorite: false,
+      favorites: [],
 
+      storage: {},
     };
+  },
+  created() {
+    const localFavoritesList = localStorage.getItem("favorites list");
+    this.storage = JSON.parse(localFavoritesList);
+    localFavoritesList;
+    for (key in this.storage) {
+      this.favorites.push(this.storage[key]);
+    }
+  },
+  components: {
+    movieItem,
   },
   methods: {
     searchMovies() {
-      if (this.search !== "") {
+      if (
+        this.search !== "" &&
+        this.year !== "" &&
+        this.movieType !== "Choose type"
+      ) {
+        // Make a request for a user with a given ID
+        axios
+          .get(
+            `http://www.omdbapi.com/?apikey=${this.API_KEY}&s=${this.search}&y=${this.year}&type=${this.movieType}`
+          )
+          .then((response) => {
+            this.movieList = response.data.Search;
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+          .then(function () {
+            // always executed
+          });
+      } else if (this.search !== "" && this.year !== "") {
+        // Make a request for a user with a given ID
+        axios
+          .get(
+            `http://www.omdbapi.com/?apikey=${this.API_KEY}&s=${this.search}&y=${this.year}`
+          )
+          .then((response) => {
+            this.movieList = response.data.Search;
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+          .then(function () {
+            // always executed
+          });
+      } else if (this.search !== "" && this.movieType !== "Choose type") {
+        // Make a request for a user with a given ID
+        axios
+          .get(
+            `http://www.omdbapi.com/?apikey=${this.API_KEY}&s=${this.search}&type=${this.movieType}`
+          )
+          .then((response) => {
+            this.movieList = response.data.Search;
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+          .then(function () {
+            // always executed
+          });
+      } else if (this.search !== "") {
         // Make a request for a user with a given ID
         axios
           .get(
@@ -31,12 +112,16 @@ const App = {
     },
     showMovieInfo() {
       this.showModal = true;
-
+    },
+    showFavoriteList() {
+      this.showFavorite = true;
     },
     getMovieInfo(movieID) {
       // Make a request for a user with a given ID
       axios
-        .get(`http://www.omdbapi.com/?apikey=${this.API_KEY}&plot=full&i=${movieID}`)
+        .get(
+          `http://www.omdbapi.com/?apikey=${this.API_KEY}&plot=full&i=${movieID}`
+        )
         .then((response) => {
           this.movieInfo = response.data;
           this.showMovieInfo();
@@ -48,6 +133,33 @@ const App = {
         .then(function () {
           // always executed
         });
+    },
+
+    addToFavorites(movieID) {
+      const movieIndex = this.movieList.findIndex(
+        (el) => el.imdbID === movieID
+      );
+      const movieIndex2 = this.favorites.findIndex(
+        (el) => el.imdbID === movieID
+      );
+
+      if (movieIndex2 === -1) {
+        this.favorites.push(this.movieList[movieIndex]);
+      } else {
+        this.favorites.splice(movieIndex2, 1);
+      }
+      localStorage.setItem("favorites list", JSON.stringify(this.favorites));
+    },
+    moviesAndFavorites() {
+      let movieArr = [];
+      this.movieList.forEach((el) => {
+        let findFav = this.favorites.find((movie) => {
+          return el.imdbID === movie.imdbID;
+        });
+        el.inFav = findFav !== undefined ? true : false;
+        movieArr.push(el);
+      });
+      return movieArr;
     },
   },
 };
